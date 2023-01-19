aab59bdd3516d0a9228cb04d6b0ac768557a163c
Medeiros <hsymetry@gmail.com>
2023-01-18 22:47:45 -0300

    Add new connectionState hadle

--- lib/app/services/auth_check.dart
+++ lib/app/services/auth_check.dart
@@ -12,7 +12,13 @@ class AuthCheck extends StatelessWidget {
     return StreamBuilder(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
-        if (snapshot.hasData) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.black87,
+            ),
+          );
+        } else if (snapshot.hasData) {
           return const HomeScreen();
         } else {
           return const LoginScreen();
