2283fa4c0a9454786eb19ebafbfd04324bf438a9
Medeiros <hsymetry@gmail.com>
2023-01-19 23:31:22 -0300

    Add dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:exclusive_diary/app/pages/login/email_login/controller/login_with_email_controller.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'app/exclusive_diary.dart';
 import 'firebase_options.dart';
@@ -8,6 +10,6 @@ main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  ).then((value) => Get.put(LoginWithEmailController()));
   runApp(const ExclusiveDiary());
 }
